6b14eca9d981f6c83c1d30c0c9eaf263439a65e1
Berzeg07 <berezg86@gmail.com>
2017-06-11 19:22:46 +0400

    11.06

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
 </head>
 <body>
 
+	<!-- Header -->
+
 	<div class="header-top">
 		<div class="container">
 			<div class="main-menu">
@@ -37,6 +39,163 @@
 		</div>
 	</div>
 
+	<!-- Header-bottom -->
+
+	<div class="header-bottom">
+		<div class="fullscreen-bg">
+
+			<video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"><source src="video/mers.mp4" type="video/mp4"></video>    
+
+			<div class="content-header">
+				<div class="header-bottom__box">
+					<h2>КОНКУРС ДЛЯ ВЕБМАСТЕРОВ LEADGID</h2>
+					<span>Работай с нами, лей без стресса - мы не скупясь даём 3 Мерседеса! </span>
+					<div class="header-bottom__box_item">
+						<span>с 1 июня 2017 по 1 марта 2018</span>
+					</div>		
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<!-- Price -->
+	<div class="price">
+		<div class="container">
+			<div class="price__title">
+				<div class="price__title_item1">
+					<span>С 1 июня по 14 июля НАЧИСЛЯЕМ БАЛЛЫ ПО ВСЕМ ОФФЕРАМ </span>
+				</div>
+				<div class="price__title_item2">
+					<h2>Разыгрываем 3 автомобиля Mercedes-Benz C-класс<br/> и гору крутых подарков </h2>
+				</div>
+			</div>
+
+			<div class="price-box__wrap">
+				<div class="car">
+					<img class="img-responsive" src="img/kar.png" alt="">
+				</div>
+				<div class="price__box price__box_1">
+
+					<div class="price__box_absolute">
+
+						<div class="price__box_txt">
+							<p>
+								<b>Один Merscedes-Benz</b><br/> 
+								будет разыгран только среди вебмастеров,<br/> вошедших в Топ-5 
+							</p>
+						</div>
+
+						<img class="img-responsive" src="img/m1.png" alt="">
+
+					</div>
+				</div>
+
+				<div class="price__box price__box_2">
+
+					<div class="price__box_absolute">
+						<img class="img-responsive" src="img/m2.png" alt="">
+
+						<div class="price__box_txt">
+							<p>
+								<b>Второй Мерседес</b> мы разыграем среди<br/> вебмастеров, занявших в рейтинге<br/> места с 6 по 20. Шансы на победу<br/> очень высоки!  
+							</p>
+						</div>
+					</div>
+
+				</div>
+
+
+				<div class="price__box price__box_3">
+
+					<div class="price__box_absolute">
+
+						<div class="price__box_txt">
+							<p>
+								Шанс выиграть <b>третий Мерседес</b><br/> получит каждый! 
+							</p>
+						</div>
+
+						<img class="img-responsive" src="img/m3.png" alt="">
+					</div>
+				</div>
+				<div class="clear"></div>
+
+			</div>
+
+		</div>
+	</div>
+
+	<!-- Footer -->
+	<div class="footer">
+		<div class="container">
+			<div class="footer__box">
+				<h2>Правила</h2>
+				<ul>
+					<li>
+
+						<span>
+							С 1 июня по 14 июля по всем офферам в Leadgid вебмастер получает баллы, количество которых равно оплате за конверсию. С 15 июля вебмастер получает баллы только по офферам, являющимся спонсором акции. Баллы по офферам платиновых спонсоров умножаются на 5, золотых спонсоров - на 3, серебряных спонсоров - на два. 
+						</span>
+
+					</li>
+					<li>
+
+						<span>
+							Топ вебмастеров формируется на основе заработанных баллов. Если у вебмастера несколько аккаунтов, баллы по ним можно объединить. 
+						</span>
+
+					</li>
+					<li>
+
+						<span>
+							о итогам конкурса вебмастер получит лотерейные билеты, количество которых пропорционально заработанным баллам. Чем больше баллов, тем больше билетов и, соответственно, выше шанс выиграть один из главных призов! 
+						</span>
+
+					</li>
+					<li>
+
+						<span>
+							Один автомобиль будет разыгран только среди Топ-5 вебмастеров. 
+						</span>
+
+					</li>
+					<li>
+
+						<span>
+							Второй автомобиль будет разыгран среди всех вебмастеров, кто набрал количество баллов хотя бы для 1 лотерейного билета. 
+						</span>
+
+					</li>
+					<li>
+
+						<span>
+							Третий автомобиль будет разыгран среди всех, за исключением вебмастеров из Топ-5, кто набрал количество баллов хотя бы для 1 лотерейного билета. 
+						</span>
+
+					</li>
+					<li>
+
+						<span>
+							Автомобили могут выиграть только те вебмастера, которые будут присутствовать на мероприятии лично, или направят своего представителя. 
+						</span>
+
+					</li>
+					<li>
+
+						<span>
+							Церемония розыгрыша и вручения всех призов будет проходить на закрытом мероприятии для вебмастеров и рекламодателей.
+						</span>
+
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="button-wrap">
+		<a href="#">стать вебмастером leadgid</a>
+	</div>
 
 
 
